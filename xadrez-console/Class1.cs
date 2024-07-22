18a4acbf16c7ce0e3e03d21429e6533150ccd536
BarrosBrendo <brendo.barros89@outlook.com>
2024-07-22 00:15:40 -0300

    Entidade Posicao Criada

--- xadrez-console/Class1.cs
+++ xadrez-console/Class1.cs
@@ -1,6 +1,12 @@
-﻿namespace Program.cs;
+﻿using tabuleiro;
+namespace Program.cs;
 
 public class Class1
 {
+    static void Main(string[] args)
+    {
+        Posicao P = new Posicao(3, 4);
 
+        Console.WriteLine("Posicao " + P );
+    }
 }
